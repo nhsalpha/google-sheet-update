449c676f131bd75ce025d720d596e74a6e4340a8
Dom Smith <dom@teneightfive.com>
2016-07-26 11:48:53 +0100

    Store sheet to be updated as env variable This allows the sheet to be renamed and updated without needing to change and redeploy any code.

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   apiBase: process.env.API_BASE,
   apiKey: process.env.API_KEY,
   sheetId: process.env.SHEET_ID,
+  sheetName: process.env.SHEET_NAME || 'Sheet1',
   googleEmail: process.env.CLIENT_EMAIL,
   googleKey: process.env.PRIVATE_KEY,
 };
